697dcdd85b72d369055ffe013a160905d1e1a2dd
fajaralfa <fajaralfrzi@gmail.com>
2026-02-01 09:48:09 +0700

    parser skeleton

--- src/asm/parser.zig
+++ src/asm/parser.zig
@@ -33,16 +33,22 @@ pub const Operand = union(enum) {
 pub const Register = enum { PC, SP, X1, X2, X3, MEPC, MCAUSE, MTVEC };
 pub const Immediate = i8;
 
+pub const ParseError = error{
+    InvalidSyntax,
+};
+
 pub const Parser = struct {
     allocator: std.mem.Allocator,
     tokens: []const lexer.Token,
     ast: Program,
+    pos: i32,
 
     fn init(allocator: std.mem.Allocator, input: []lexer.Token) Parser {
         return Parser{
             .allocator = allocator,
             .tokens = input,
             .ast = Program.empty,
+            .pos = 0,
         };
     }
 
@@ -50,9 +56,70 @@ pub const Parser = struct {
         self.ast.deinit(self.allocator);
     }
 
+    fn print(self: *Parser) void {
+        for (self.ast.items) |block| {
+            std.log.debug("{}", block);
+        }
+    }
+
     fn parse(self: *Parser) !Program {
-        try self.ast.append(self.allocator, Block{ .label = .{ .name = "anjay", .address = 123 } });
+        while (self.current().type != .EOF) {
+            self.ast.append(self.allocator, try self.parse_block());
+        }
         return self.ast;
+    }
+
+    fn current(self: *Parser) *lexer.Token {
+        return self.tokens[self.pos];
+    }
+
+    fn advance(self: *Parser) void {
+        self.pos += 1;
+    }
+
+    fn expect(self: *Parser, toktype: lexer.TokenType) !lexer.Token {
+        const tok = self.current();
+        if (tok.type != toktype) {
+            return ParseError.InvalidSyntax;
+        }
+        self.advance();
+        return tok;
+    }
+
+    fn parseBlock(self: *Parser) !Block {
+        if (self.current().type == .Identifier and self.tokens[self.pos + 1].type == .Colon) {
+            const label = self.parseLabel();
+            try self.expect(.Colon);
+            return Block{ .label = label };
+        } else {
+            const instr = try self.parseInstr();
+            return Block{ .instruction = instr };
+        }
+    }
+
+    fn parseInstr(self: *Parser) !Instruction {
+        const opcode = (try self.expect(.Identifier)).literal;
+        var operand = std.ArrayList(Operand).empty;
+        switch (self.current().type) {
+            .Identifier, .ImmVal => {
+                operand.append(self.allocator, try self.parseOperand());
+                while (self.current().type == .Comma) {
+                    self.advance();
+                    operand.append(self.allocator, try self.parseOperand());
+                }
+            },
+        }
+        return Instruction{ .opcode = opcode, .operand = operand.items };
+    }
+
+    fn parseOperand(self: *Parser) !Operand {
+        const tok = self.current();
+        if (tok.type == .Identifier) {
+            self.advance();
+            return Operand{ .immediate = 20 };
+        }
+
+        return ParseError.InvalidSyntax;
     }
 };
 
