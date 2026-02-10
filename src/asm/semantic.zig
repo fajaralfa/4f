2bf1a8fc4beedb40aa9520ebfda8df810660c50a
fajaralfa <fajaralfrzi@gmail.com>
2026-02-10 23:03:54 +0700

    add all opcode semantic analyzer (placeholder only)

--- src/asm/semantic.zig
+++ src/asm/semantic.zig
@@ -18,6 +18,17 @@ pub const SemanticAnalyzer = struct {
     program: asttype.Program,
 
     fn init(allocator: std.mem.Allocator, program: asttype.Program) SemanticAnalyzer {
+        const opcode_specs: [INSTR_LEN]OperandSpec = getOpcodeSpec();
+
+        return SemanticAnalyzer{
+            .allocator = allocator,
+            .labels = std.StringHashMap(usize).init(allocator),
+            .opcode_specs = opcode_specs,
+            .program = program,
+        };
+    }
+
+    inline fn getOpcodeSpec() [INSTR_LEN]OperandSpec {
         var opcode_specs: [INSTR_LEN]OperandSpec = undefined;
         inline for (0..INSTR_LEN) |i| {
             opcode_specs[i] = .{
@@ -33,12 +44,103 @@ pub const SemanticAnalyzer = struct {
             .instr_fn = lw,
         };
 
-        return SemanticAnalyzer{
-            .allocator = allocator,
-            .labels = std.StringHashMap(usize).init(allocator),
-            .opcode_specs = opcode_specs,
-            .program = program,
+        opcode_specs[2] = .{
+            .count = 3,
+            .kinds = &.{ .register, .register, .immediate },
+            .instr_fn = sw,
         };
+
+        opcode_specs[3] = .{
+            .count = 2,
+            .kinds = &.{ .register, .immediate },
+            .instr_fn = lui,
+        };
+
+        opcode_specs[4] = .{
+            .count = 3,
+            .kinds = &.{ .register, .register, .immediate },
+            .instr_fn = addi,
+        };
+
+        opcode_specs[5] = .{
+            .count = 3,
+            .kinds = &.{ .register, .register, .register },
+            .instr_fn = add,
+        };
+
+        opcode_specs[6] = .{
+            .count = 3,
+            .kinds = &.{ .register, .register, .register },
+            .instr_fn = sub,
+        };
+
+        opcode_specs[7] = .{
+            .count = 3,
+            .kinds = &.{ .register, .register, .register },
+            .instr_fn = opAnd,
+        };
+
+        opcode_specs[8] = .{
+            .count = 3,
+            .kinds = &.{ .register, .register, .register },
+            .instr_fn = opNot,
+        };
+
+        opcode_specs[9] = .{
+            .count = 3,
+            .kinds = &.{ .register, .register, .register },
+            .instr_fn = opOr,
+        };
+
+        opcode_specs[10] = .{
+            .count = 3,
+            .kinds = &.{ .register, .register, .register },
+            .instr_fn = opXor,
+        };
+
+        opcode_specs[11] = .{
+            .count = 3,
+            .kinds = &.{ .register, .register, .register },
+            .instr_fn = opSll,
+        };
+
+        opcode_specs[12] = .{
+            .count = 3,
+            .kinds = &.{ .register, .register, .register },
+            .instr_fn = opSrl,
+        };
+
+        opcode_specs[13] = .{
+            .count = 3,
+            .kinds = &.{ .register, .register, .register },
+            .instr_fn = opSra,
+        };
+
+        opcode_specs[14] = .{
+            .count = 3,
+            .kinds = &.{.register},
+            .instr_fn = jr,
+        };
+
+        opcode_specs[15] = .{
+            .count = 3,
+            .kinds = &.{ .register, .register, .register },
+            .instr_fn = beq,
+        };
+
+        opcode_specs[16] = .{
+            .count = 3,
+            .kinds = &.{ .register, .register, .register },
+            .instr_fn = bne,
+        };
+
+        opcode_specs[0x1f] = .{
+            .count = 3,
+            .kinds = &.{},
+            .instr_fn = halt,
+        };
+
+        return opcode_specs;
     }
 
     fn deinit(self: *SemanticAnalyzer) void {
@@ -93,13 +195,58 @@ pub const SemanticAnalyzer = struct {
 
     fn lw(self: *SemanticAnalyzer, instr: *asttype.Instruction) !void {
         _ = self;
-        // _ = instr;
         const items = instr.operand.items;
         const imm: asttype.Immediate = getOperand(.immediate, items[2]);
         if (imm > std.math.maxInt(u5)) {
             return SemanticError.ImmediateSizeExceeded;
         }
     }
+
+    fn sw(self: *SemanticAnalyzer, instr: *asttype.Instruction) !void {
+        _ = self;
+        const items = instr.operand.items;
+        const imm: asttype.Immediate = getOperand(.immediate, items[2]);
+        if (imm > std.math.maxInt(u5)) {
+            return SemanticError.ImmediateSizeExceeded;
+        }
+    }
+
+    fn lui(self: *SemanticAnalyzer, instr: *asttype.Instruction) !void {
+        _ = self;
+        const items = instr.operand.items;
+        const imm: asttype.Immediate = getOperand(.immediate, items[1]);
+        if (imm > std.math.maxInt(u8)) {
+            return SemanticError.ImmediateSizeExceeded;
+        }
+    }
+
+    fn addi(self: *SemanticAnalyzer, instr: *asttype.Instruction) !void {
+        _ = self;
+        const items = instr.operand.items;
+        const imm: asttype.Immediate = getOperand(.immediate, items[2]);
+        if (imm > std.math.maxInt(u5)) {
+            return SemanticError.ImmediateSizeExceeded;
+        }
+    }
+
+    fn add(_: *SemanticAnalyzer, _: *asttype.Instruction) !void {}
+    fn sub(_: *SemanticAnalyzer, _: *asttype.Instruction) !void {}
+    fn opAnd(_: *SemanticAnalyzer, _: *asttype.Instruction) !void {}
+    fn opNot(_: *SemanticAnalyzer, _: *asttype.Instruction) !void {}
+    fn opOr(_: *SemanticAnalyzer, _: *asttype.Instruction) !void {}
+    fn opXor(_: *SemanticAnalyzer, _: *asttype.Instruction) !void {}
+    fn opSll(_: *SemanticAnalyzer, _: *asttype.Instruction) !void {}
+    fn opSrl(_: *SemanticAnalyzer, _: *asttype.Instruction) !void {}
+    fn opSra(_: *SemanticAnalyzer, _: *asttype.Instruction) !void {}
+
+    fn jr(self: *SemanticAnalyzer, instr: *asttype.Instruction) !void {
+        _ = self;
+        _ = instr;
+    }
+
+    fn beq(_: *SemanticAnalyzer, _: *asttype.Instruction) !void {}
+    fn bne(_: *SemanticAnalyzer, _: *asttype.Instruction) !void {}
+    fn halt(_: *SemanticAnalyzer, _: *asttype.Instruction) !void {}
 };
 
 fn OperandType(comptime kind: asttype.OperandKind) type {
@@ -110,10 +257,7 @@ fn OperandType(comptime kind: asttype.OperandKind) type {
     };
 }
 
-fn getOperand(
-    comptime kind: asttype.OperandKind,
-    op: asttype.Operand,
-) OperandType(kind) {
+fn getOperand(comptime kind: asttype.OperandKind, op: asttype.Operand) OperandType(kind) {
     return switch (op) {
         kind => |v| v,
         else => unreachable,
@@ -125,6 +269,14 @@ const OperandSpec = struct {
     kinds: []const asttype.OperandKind,
     instr_fn: *const fn (*SemanticAnalyzer, *asttype.Instruction) anyerror!void,
 };
+
+fn createOperands(allocator: std.mem.Allocator, data: []asttype.Operand) !std.ArrayList(asttype.Operand) {
+    var operands = std.ArrayList(asttype.Operand).empty;
+    for (data) |d| {
+        try operands.append(allocator, d);
+    }
+    return operands;
+}
 
 test "init semantic analyzer" {
     const allocator = std.testing.allocator;
@@ -140,6 +292,89 @@ test "init semantic analyzer" {
     try operand.append(allocator, .{ .register = .X1 });
     try operand.append(allocator, .{ .immediate = 31 });
     try program.append(allocator, asttype.Block{ .instruction = asttype.Instruction{ .opcode = .Lw, .operand = operand } });
+
+    var sema = SemanticAnalyzer.init(allocator, program);
+    defer sema.deinit();
+    try sema.analyze();
+    for (program.items) |ins| {
+        ins.print();
+    }
+}
+
+test "lw" {
+    const allocator = std.testing.allocator;
+
+    var program = try asttype.Program.initCapacity(allocator, 1);
+    defer program.deinit(allocator);
+
+    var operandSource = [_]asttype.Operand{
+        .{ .register = .X1 },
+        .{ .register = .X1 },
+        .{ .immediate = 31 },
+    };
+    var operands = try createOperands(allocator, operandSource[0..]);
+    defer operands.deinit(allocator);
+    try program.append(allocator, asttype.Block{
+        .instruction = asttype.Instruction{
+            .opcode = .Lw,
+            .operand = operands,
+        },
+    });
+
+    var sema = SemanticAnalyzer.init(allocator, program);
+    defer sema.deinit();
+    try sema.analyze();
+    for (program.items) |ins| {
+        ins.print();
+    }
+}
+
+test "sw" {
+    const allocator = std.testing.allocator;
+
+    var program = try asttype.Program.initCapacity(allocator, 1);
+    defer program.deinit(allocator);
+
+    var operandSource = [_]asttype.Operand{
+        .{ .register = .X1 },
+        .{ .register = .X1 },
+        .{ .immediate = 31 },
+    };
+    var operands = try createOperands(allocator, operandSource[0..]);
+    defer operands.deinit(allocator);
+    try program.append(allocator, asttype.Block{
+        .instruction = asttype.Instruction{
+            .opcode = .Sw,
+            .operand = operands,
+        },
+    });
+
+    var sema = SemanticAnalyzer.init(allocator, program);
+    defer sema.deinit();
+    try sema.analyze();
+    for (program.items) |ins| {
+        ins.print();
+    }
+}
+
+test "lui" {
+    const allocator = std.testing.allocator;
+
+    var program = try asttype.Program.initCapacity(allocator, 1);
+    defer program.deinit(allocator);
+
+    var operandSource = [_]asttype.Operand{
+        .{ .register = .X1 },
+        .{ .immediate = 0xff },
+    };
+    var operands = try createOperands(allocator, operandSource[0..]);
+    defer operands.deinit(allocator);
+    try program.append(allocator, asttype.Block{
+        .instruction = asttype.Instruction{
+            .opcode = .Lui,
+            .operand = operands,
+        },
+    });
 
     var sema = SemanticAnalyzer.init(allocator, program);
     defer sema.deinit();
